200cda4c8644b9cc3cd2478ce399acfd3f6c4730
shangxiaaabb <huangjie20011001@163.com>
2025-03-26 22:34:45 +0800

    a

--- _jupyter/PythoThreading.py
+++ _jupyter/PythoThreading.py
@@ -1,17 +1,67 @@
-import multiprocessing
+import time
 
-class SquareCalculator:
-    def __init__(self, factor=2):
-        self.factor = factor  # 使实例方法非静态，防止被 pickle
+def com_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # 记录开始时间
+        result = func(*args, **kwargs)  # 执行被装饰的函数
+        print(f"Used Time: {time.time() - start_time:.4f} 秒")  # 计算并输出执行时间
+        return result
+    return wrapper
 
-    def square(self, number):
-        return number ** self.factor  # 这里 self 使其不可被 pickle
+@com_time
+def test():
+    time.sleep(0.5)
+    print("Hello!")
 
-if __name__ == "__main__":
-    calculator = SquareCalculator()  # 创建类实例
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def main():
+    test()
 
-    with multiprocessing.Pool(processes=4) as pool:
-        results = pool.map(calculator.square, numbers)  # 这里会报错，无法 pickle
-    print("原始列表:", numbers)
-    print("平方结果:", results)
+# def main():
+#     start_time = time.time()
+#     test()
+#     print(f"Used Time: {time.time()- start_time}")
+
+class Test():
+    def __init__(self, age):
+        self.age = age
+    
+    def add(self):
+        '''加一'''
+        return self.age+ 1
+
+# test = Test(13)
+# test.__dict__['name'] = 'hjie'
+# print(test.name)
+# print(test.add.__name__)
+# print(test.add.__doc__)
+
+
+class Person:
+    place= 'bj' # 类变量（所有实例共享）
+
+    def __init__(self, name):
+        self.name = name
+    
+    @staticmethod
+    def age1(age):
+        print(f"{age}")
+    
+    @classmethod
+    def new_place(cls, new):
+        cls.place = new
+
+    def age2(self, age):
+        print(f"{self.name}:{age} from {self.place}")
+    
+    def age3(self, age):
+        if age>= 20:
+            Person.new_place('cs')
+        print(f"{self.name}:{age} from {self.__class__.place}")
+
+Person.age1(13)
+Person("Tom").age2(13)
+Person("Tom").age3(23)
+
+import torch
+
+# with torch.no_grad()
